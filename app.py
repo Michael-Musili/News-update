9ac7d8a7b2123c2d42e3cb533a319d07d959a21c
Michael-Musili <michaelmuttuku@gmail.com>
2022-03-10 13:17:00 +0300

    added start.sh file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from newsapi import NewsApiClient
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ def index():
 
     mylist = zip(news, desc, img)
 
-    return render_template('index.html',context=mylist)
+    return render_template('index.html', context=mylist)
 
 
 if __name__ == "__main__":
