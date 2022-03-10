3fccfd3421b1179a595504452e8bad82fef84fd7
Michael-Musili <michaelmuttuku@gmail.com>
2022-03-10 14:28:39 +0300

    added style

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 from newsapi import NewsApiClient
 
 app = Flask(__name__)
@@ -18,15 +18,15 @@ def index():
     img = []
 
     for i in range(len(articles)):
-        articles = articles[i]
+        myarticles = articles[i]
 
-        news.append(articles['title'])
-        desc.append(articles['description'])
-        img.append(articles['urlToImage'])
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
 
     mylist = zip(news, desc, img)
 
-    return render_template('index.html', context=mylist)
+    return render_template('index.html',context=mylist)
 
 
 if __name__ == "__main__":
