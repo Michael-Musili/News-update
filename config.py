9e7fbbbe7c2f9c4272edb78dd2d2f5e85c0a211b
Michael-Musili <michaelmuttuku@gmail.com>
2022-03-14 06:21:44 +0300

    added news_api_keys

---
+++ config.py
@@ -0,0 +1,34 @@
+
+import os
+
+
+class Config:
+    '''
+    Global configuration of the parent
+    '''
+
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    NEWS_API_SOURCE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    '''
+    Production configuration child
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child
+    '''
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
