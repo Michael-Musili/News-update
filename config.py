0ab841683022a68936becea71f9532c495d5f24d
Michael-Musili <michaelmuttuku@gmail.com>
2022-03-16 14:06:24 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ class Config:
 
     SOURCES_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
     ARTICLES_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    NEWS_API_KEY = 'f4c86580284a4e12bd792c731cfe34c4'
+    NEWS_API_KEY = '83f8de393be94ef8ad50bd4517fb123f'
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    
 
 
 class ProdConfig(Config):
